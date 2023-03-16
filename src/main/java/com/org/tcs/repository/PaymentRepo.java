11f633458535cfaed4da99a1e8aafd22f353533f
Mounikagullipalli <114406170+Mounikagullipalli@users.noreply.github.com>
2023-03-15 22:27:57 -0700

    Update PaymentRepo.java

--- src/main/java/com/org/tcs/repository/PaymentRepo.java
+++ src/main/java/com/org/tcs/repository/PaymentRepo.java
@@ -1,7 +1,6 @@
 package com.org.tcs.repository;
 
 import com.org.tcs.model.Payment;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
